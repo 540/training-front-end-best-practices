16ed8050c7f6c05a5dba784c7aeeb22d19656236
Mikel Ros <mikel.ros@540deg.com>
2023-11-14 10:56:09 +0100

    Crea primera página

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dulces-petalos.herokuapp.com',
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
