f5fdc2ecbbdec8a297c76bf8db06ba798a5617cd
Mikel Ros <mikel.ros@540deg.com>
2023-11-17 10:51:26 +0100

    Ajustes varios

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -1,13 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (Math.random() <= 0.3) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const forceError = false
+  if (forceError) {
     res.status(422).json({ code: 'FLOWERS_NOT_FOUND' })
   } else {
-    fetch('https://dulces-petalos.herokuapp.com/api/product')
-      .then(response => response.json())
-      .then(data => {
-        res.status(200).json(data)
-      })
+    const response = await fetch(
+      'https://dulces-petalos.herokuapp.com/api/product',
+    )
+    const data = await response.json()
+    res.status(200).json(data)
   }
 }
